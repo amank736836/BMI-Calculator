b0962d0531f971543965704949d0ab996634f7f8
Aman Kumar <amankarguwal0@gmail.com>
2024-05-21 13:46:30 +0530

    public folder added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const fs = require("fs");
 
-app.use(express.static("."));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
